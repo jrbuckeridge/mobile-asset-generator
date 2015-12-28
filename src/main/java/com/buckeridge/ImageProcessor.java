596a60edaf01f294b136237256041ae6cdf1bd6f
Junior Buckeridge Amargo <buckeridge85@gmail.com>
2015-12-27 18:33:47 -0800

    high quality, antialiasing and bicubic interpolation

--- src/main/java/com/buckeridge/ImageProcessor.java
+++ src/main/java/com/buckeridge/ImageProcessor.java
@@ -46,6 +46,9 @@ public class ImageProcessor {
         if (preserveAlpha) {
             g.setComposite(AlphaComposite.Src);
         }
+        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
         g.drawImage(inputImage, 0, 0, scaledWidth, scaledHeight, null);
         g.dispose();
         //write output image
